52ed133e0d2a216d7e0f68bdc2c61b78698bcd39
Sheep-heee <sheephi0609@gmail.com>
2024-07-03 18:31:07 +0900

    4th

--- 240712_1P_My_Profile/Project/script.js
+++ 240712_1P_My_Profile/Project/script.js
@@ -1,6 +1,51 @@
+// scroll header fixed
+window.addEventListener("scroll", () => {
+  const navTitle = document.querySelector(".nav_title");
+  const logo = document.querySelector(".nav_logo");
+
+  if (window.scrollY > 80) {
+    navTitle.classList.add("active");
+    logo.classList.add("active");
+  } else {
+    navTitle.classList.remove("active");
+    logo.classList.remove("active");
+  }
+});
+
 // toggle
 const toggleBtn = document.querySelector(".fa-bars");
 toggleBtn.addEventListener("click", function () {
-  const gnbMobile = document.querySelector(".top_menu");
-  gnbMobile.classList.toggle("open");
+  const gnb = document.querySelector(".top_menu");
+  gnb.classList.toggle("open");
+});
+
+// personality desc trigger
+const personalityLeftBtn = document.querySelector(
+  ".personality_icon_emoji:first-child"
+);
+const personalityRightBtn = document.querySelector(
+  ".personality_icon_emoji:nth-child(2)"
+);
+const personalityLeftResult = document.querySelector(".personality_desc.left");
+const personalityRightResult = document.querySelector(
+  ".personality_desc.right"
+);
+const personalityLeftClose = document.querySelector("");
+const personalityRightClose = document.querySelector("");
+
+personalityLeftBtn.addEventListener("click", () => {
+  console.log(click);
+  personalityLeftResult.classList.add("active");
+});
+
+personalityRightBtn.addEventListener("click", () => {
+  console.log(click);
+  personalityRightResult.classList.add("active");
+});
+
+personalityLeftClose.addEventListener("click", () => {
+  personalityLeftResult.remove("active");
+});
+personalityRightClose.addEventListener("click", () => {
+  personalityRightResult.remove("active");
 });
