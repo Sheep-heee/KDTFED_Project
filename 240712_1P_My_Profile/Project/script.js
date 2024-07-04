582802d617795b290a1ecc8e6394a428d53b786b
Sheep-heee <sheephi0609@gmail.com>
2024-07-04 18:22:55 +0900

    5th

--- 240712_1P_My_Profile/Project/script.js
+++ 240712_1P_My_Profile/Project/script.js
@@ -30,22 +30,64 @@ const personalityLeftResult = document.querySelector(".personality_desc.left");
 const personalityRightResult = document.querySelector(
   ".personality_desc.right"
 );
-const personalityLeftClose = document.querySelector("");
-const personalityRightClose = document.querySelector("");
+const personalityLeftClose = document.querySelector(
+  ".personality_desc.left>.personality_desc_close"
+);
+const personalityRightClose = document.querySelector(
+  ".personality_desc.right>.personality_desc_close"
+);
 
 personalityLeftBtn.addEventListener("click", () => {
-  console.log(click);
   personalityLeftResult.classList.add("active");
 });
-
 personalityRightBtn.addEventListener("click", () => {
-  console.log(click);
   personalityRightResult.classList.add("active");
 });
-
 personalityLeftClose.addEventListener("click", () => {
-  personalityLeftResult.remove("active");
+  personalityLeftResult.classList.remove("active");
 });
 personalityRightClose.addEventListener("click", () => {
-  personalityRightResult.remove("active");
+  personalityRightResult.classList.remove("active");
 });
+
+// personality slide
+const bgImgs = ["slide_1.png", "slide_2.png", "slide_3.png"];
+const bgImg = document.querySelector(".personality_slide_img");
+bgImg.style.backgroundImage = `url(//ecimg.cafe24img.com/pg1108b23660447004/sheephi0609/1P_My_Profile/${bgImgs[0]})`;
+const slideContents = document.querySelector(".personality_slide_text");
+const slideArrows = document.querySelectorAll(".personality_slide_btn");
+
+const topContents = document.querySelector(".personality_slide_text");
+const conetentTit = topContents.querySelector(".personality_slide_text > h1");
+const conetentDesc = topContents.querySelector(".personality_slide_text > p");
+
+fetch("./data.json")
+  .then((response) => response.json())
+  .then((jsonData) => {
+    const [firstData, ...otherData] = jsonData.data;
+
+    topContents.style.backgroundColor = `#c4e7f6`;
+    conetentTit.innerText = firstData.title;
+    conetentDesc.innerText = firstData.description;
+
+    let i = 0;
+    slideArrows.forEach((item) => {
+      item.addEventListener("click", (e) => {
+        e.preventDefault();
+        i++;
+        if (i > bgImgs.length - 1) {
+          i = 0;
+          bgImg.style.backgroundImage = `url(//ecimg.cafe24img.com/pg1108b23660447004/sheephi0609/1P_My_Profile/${bgImgs[i]})`;
+        }
+        bgImg.style.backgroundImage = `url(//ecimg.cafe24img.com/pg1108b23660447004/sheephi0609/1P_My_Profile/${bgImgs[i]})`;
+        if (i === 1) {
+          topContents.style.backgroundColor = `#ffe9c9`;
+        } else {
+          topContents.style.backgroundColor = `#c4e7f6`;
+        }
+        const { title, description } = jsonData.data[i];
+        conetentTit.innerText = title;
+        conetentDesc.innerText = description;
+      });
+    });
+  });
